5d716aa7c49a43dfd688fc70cbd6122ede3282f0
rodrigosm95 <rodrigo.sanmartin95@gmail.com>
2022-01-08 20:46:57 -0300

    primeros estilos al product detail

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/product_detail.html'))
 });
 
 app.listen(3000, () => {
