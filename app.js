7cbb503e68e3a42b2ac327fb6ec044de097c8186
JulianMartinezx <55161409+JulianMartinezx@users.noreply.github.com>
2022-01-10 05:48:27 -0300

    Solucionando error en la ruta del index.
    Cambiar "home" por "index"

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const publicPath = path.resolve(__dirname, './public');
 app.use(express.static(publicPath));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'))
+    res.sendFile(path.join(__dirname, '/views/index.html'))
 });
 
 app.get('/product_detail', (req, res) => {
